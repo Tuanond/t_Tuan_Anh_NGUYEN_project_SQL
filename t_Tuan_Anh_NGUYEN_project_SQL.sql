15756e6436b6c71eb6635897e327b907e4610485
Tuan Anh Nguyen <ondra.nguyen@gmail.com>
2024-06-17 20:56:36 +0200

    init load

---
+++ t_Tuan_Anh_NGUYEN_project_SQL.sql
@@ -0,0 +1,58 @@
+-- PROJEKT 1
+
+-- Výzkumné otázky
+-- Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
+-- Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
+-- Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
+-- Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)?
+-- Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednom roce, projeví se to na cenách potravin či mzdách ve stejném nebo násdujícím roce výraznějším růstem?
+
+-- Primární tabulky:
+
+-- czechia_payroll – Informace o mzdách v různých odvětvích za několikaleté období. Datová sada pochází z Portálu otevřených dat ČR.
+-- czechia_payroll_calculation – Číselník kalkulací v tabulce mezd.
+-- czechia_payroll_industry_branch – Číselník odvětví v tabulce mezd.
+-- czechia_payroll_unit – Číselník jednotek hodnot v tabulce mezd.
+-- czechia_payroll_value_type – Číselník typů hodnot v tabulce mezd.
+-- czechia_price – Informace o cenách vybraných potravin za několikaleté období. Datová sada pochází z Portálu otevřených dat ČR.
+-- czechia_price_category – Číselník kategorií potravin, které se vyskytují v našem přehledu.
+
+SELECT *
+FROM czechia_payroll AS cp ;
+
+SELECT *
+FROM czechia_payroll_calculation AS cpc;
+
+SELECT *
+FROM czechia_payroll_industry_branch AS cpib;
+
+SELECT *
+FROM czechia_payroll_unit AS cpu;
+
+SELECT *
+FROM czechia_payroll_value_type AS cpvt;
+
+SELECT *
+FROM czechia_price AS cp ;
+
+SELECT *
+FROM czechia_price_category AS cpc;
+
+-- Číselníky sdílených informací o ČR:
+
+czechia_region – Číselník krajů České republiky dle normy CZ-NUTS 2.
+czechia_district – Číselník okresů České republiky dle normy LAU.
+
+-- Dodatečné tabulky:
+
+-- countries - Všemožné informace o zemích na světě, například hlavní město, měna, národní jídlo nebo průměrná výška populace.
+-- economies - HDP, GINI, daňová zátěž, atd. pro daný stát a rok.
+
+
+-- STEP 1 - propojení tabulek czechia_payroll + czechia_price
+
+SELECT
+	*
+FROM czechia_payroll AS cpay
+LEFT JOIN czechia_price AS cp
+	ON cpay.payroll_year = YEAR(cp.date_from);
